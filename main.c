93f6b1fc8f6ec97f61a18595023485f6d03d8b7e
ArielAviad <aviadrel@gmail.com>
2016-08-25 17:26:10 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "tic_tac_too.h"
+
+int main() {
+    return run_tic_tac_to();
+}
---
+++ tic_tac_too.h
@@ -0,0 +1,6 @@
+
+#ifndef TIC_TAC_TO_TIC_TAC_TOO_H
+#define TIC_TAC_TO_TIC_TAC_TOO_H
+/*run the game.*/
+int run_tic_tac_to();
+#endif /*TIC_TAC_TO_TIC_TAC_TOO_H*/
